1ce03ca1d8021a2d6843816c5f91fd196306d8a8
PORTATIL <PORTATIL@DESKTOP-6IOTAUL>
2019-09-27 16:19:25 +0200

    Fix

---
+++ build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,10 @@
+build/Debug/Cygwin-Windows/main.o: main.cpp CargarFichero.h Algoritmos.h \
+ Parametros.h timer.h
+
+CargarFichero.h:
+
+Algoritmos.h:
+
+Parametros.h:
+
+timer.h:
---
+++ build/Debug/Cygwin-Windows/timer.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/timer.o: timer.c timer.h
+
+timer.h:
